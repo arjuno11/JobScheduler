d55eb4d6ca54c82ebc825a0c8da271a785b67d73
Aris <aris.purnomo.tif18@polban.ac.id>
2020-04-30 16:01:26 +0700

    initial commit

--- app/src/main/java/com/aris/jobscheduler/MainActivity.java
+++ app/src/main/java/com/aris/jobscheduler/MainActivity.java
@@ -6,6 +6,7 @@ import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.ComponentName;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.RadioGroup;
 import android.widget.SeekBar;
@@ -16,89 +17,36 @@ import android.widget.Toast;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
-    private RadioGroup networkOptions;
-    private Switch mDeviceIdleSwitch;
-    private Switch mDeviceChargingSwitch;
-    private SeekBar mSeekBar;
-    private JobScheduler mScheduler;
     private static final int JOB_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        networkOptions = findViewById(R.id.networkOptions);
-        mDeviceIdleSwitch = findViewById(R.id.idleSwitch);
-        mDeviceChargingSwitch = findViewById(R.id.chargingSwitch);
-        mSeekBar = findViewById(R.id.seekBar);
-        final TextView seekBarProgress = findViewById(R.id.seekBarProgress);
-
-        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                if (progress > 0){
-                    seekBarProgress.setText(String.format(Locale.ENGLISH, "%d s", progress));
-                }
-                else {
-                    seekBarProgress.setText(R.string.not_set);
-                }
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
     }
 
-    public void scheduleJob(View view) {
-        int selectedNetworkID = networkOptions.getCheckedRadioButtonId();
-        int selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
-        int seekBarInteger = mSeekBar.getProgress();
-        boolean seekBarSet = seekBarInteger > 0;
-        switch (selectedNetworkID) {
-            case R.id.noNetwork:
-                selectedNetworkOption = JobInfo.NETWORK_TYPE_NONE;
-                break;
-            case R.id.anyNetwork:
-                selectedNetworkOption = JobInfo.NETWORK_TYPE_ANY;
-                break;
-            case R.id.wifiNetwork:
-                selectedNetworkOption = JobInfo.NETWORK_TYPE_UNMETERED;
-                break;
-        }
+    public void scheduleJob(View v) {
+        Log.d("TAG", "Job scheduled");
+        ComponentName componentName = new ComponentName(this, NotificationJobService.class);
+        JobInfo info = new JobInfo.Builder(JOB_ID, componentName)
+                .setRequiresCharging(true)
+                .setRequiredNetworkType(JobInfo.NETWORK_TYPE_UNMETERED)
+                .setPersisted(true)
+                .setPeriodic(15 * 60 * 1000)
+                .build();
 
-        mScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
-        ComponentName componentName = new ComponentName(getPackageName(), SleeperJobScheduler.class.getName());
-        JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, componentName);
-        builder.setRequiredNetworkType(selectedNetworkOption)
-                .setRequiresDeviceIdle(mDeviceIdleSwitch.isChecked())
-                .setRequiresCharging(mDeviceChargingSwitch.isChecked());
-        if (seekBarSet){
-            builder.setOverrideDeadline(seekBarInteger * 1000);
-        }
-        boolean constraintSet = (selectedNetworkOption != JobInfo.NETWORK_TYPE_NONE) || mDeviceChargingSwitch.isChecked() || mDeviceIdleSwitch.isChecked() || seekBarSet;
-        if (constraintSet) {
-            JobInfo myJobInfo = builder.build();
-            mScheduler.schedule(myJobInfo);
-            Toast.makeText(this, "Job Scheduled, job will run when " + "the constraints are met.", Toast.LENGTH_SHORT).show();
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        int resultCode = scheduler.schedule(info);
+        if (resultCode == JobScheduler.RESULT_SUCCESS) {
+            Log.d("TAG", "Job scheduled");
         } else {
-            Toast.makeText(this, "Please set at least one constraint", Toast.LENGTH_SHORT).show();
+            Log.d("TAG", "Job scheduling failed");
         }
     }
 
-    public void cancelJobs(View view) {
-        if (mScheduler != null) {
-            mScheduler.cancelAll();
-            mScheduler = null;
-            Toast.makeText(this, "Job cancelled", Toast.LENGTH_SHORT).show();
-
-        }
+    public void cancelJobs(View v) {
+        JobScheduler scheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        scheduler.cancel(JOB_ID);
+        Log.d("TAG", "Job cancelled");
     }
 }
